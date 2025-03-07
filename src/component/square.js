6b9c27437c66cc4923a7d6a4fb8bce06f75cb897
SaihajMann <saihajmann07@gmail.com>
2025-03-07 12:15:48 -0700

    better board display

--- src/component/square.js
+++ src/component/square.js
@@ -1,6 +1,20 @@
 'use client'
 
+import { useEffect } from "react"
+
 export default function Square({ prop, onClickFunction }) {
+    var display = prop
+
+    useEffect(() => {
+        if(prop == "" || prop == undefined){
+            display = "00"
+        }
+        else{
+            display = prop
+        }
+
+    },[prop])
+
     return (
-        <button  onClick={onClickFunction} className="square">{prop}</button>
+        <button  onClick={onClickFunction} className="square">-{prop}</button>
     )}
