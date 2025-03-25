d7288d1c11d37f665cc2188844a279a134cc2fa2
SaihajMann <saihajmann07@gmail.com>
2025-03-24 21:54:36 -0600

    better board

--- src/component/square.js
+++ src/component/square.js
@@ -3,14 +3,41 @@
 import { useEffect } from "react"
 
 export default function Square({ prop, onClickFunction, number = 0, selected = 64, row=0 }) {
+    let bgColor;
+    let textColour;
+    let display = prop
+
+    if(display == ""){
+        display = "-"
+    }
+
+    if (number === selected) {
+        bgColor = "yellow";
+        textColour = "black"
+    } else if ((number + row) % 2 === 0) {
+        bgColor = "black";
+        if(prop == ""){
+            textColour = "black"
+        }else{
+            textColour = "white"
+        }
+    } else {
+        bgColor = "white";
+        if(prop == ""){
+            textColour = "white"
+        }else{
+            textColour = "black"
+        }
+    }
+
     const buttonStyle = {
-        backgroundColor: (number + row) % 2 === 0 ? 'black' : 'white',
-        color: (number + row) % 2 === 0 ? 'white' : 'black',
+        backgroundColor: bgColor,
+        color: textColour,
     };
     
     return (
         <button onClick={onClickFunction} style={buttonStyle} className="square">
-            -{prop}
+            {display}
         </button>
     );
 }
