285cc3aaf0722f80659018983adb0e9ddbbc77d0
SaihajMann <saihajmann07@gmail.com>
2025-04-04 17:04:37 -0600

    size control

--- src/component/square.js
+++ src/component/square.js
@@ -35,24 +35,24 @@ export default function Square({ prop, onClickFunction, number = 0, selected = 6
         color: textColour,
     };
 
-    const pieceWidth = 25;
-    const pieceHeight = 25;
+    const pieceWidth = 50;
+    const pieceHeight = 50;
     
     return (
         <button onClick={onClickFunction} style={buttonStyle} className="square">
             {display === "-" && display}
-            {display === "WK" && <Image src="/WhiteKing.png" alt={display} width={50} height={50} />}
-            {display === "WQ" && <Image src="/WhiteQueen.png" alt={display} width={50} height={50} />}
-            {display === "WB" && <Image src="/WhiteBishop.png" alt={display} width={50} height={50} />}
-            {display === "WR" && <Image src="/WhiteRook.png" alt={display} width={50} height={50} />}
-            {display === "WN" && <Image src="/WhiteKnight.png" alt={display} width={50} height={50} />}
-            {display === "WP" && <Image src="/WhitePawn.png" alt={display} width={50} height={50} />}
-            {display === "BK" && <Image src="/BlackKing.png" alt={display} width={50} height={50} />}
-            {display === "BQ" && <Image src="/BlackQueen.png" alt={display} width={50} height={50} />}
-            {display === "BB" && <Image src="/BlackBishop.png" alt={display} width={50} height={50} />}
-            {display === "BR" && <Image src="/BlackRook.png" alt={display} width={50} height={50} />}
-            {display === "BN" && <Image src="/BlackKnight.png" alt={display} width={50} height={50} />}
-            {display === "BP" && <Image src="/BlackPawn.png" alt={display} width={50} height={50} />}
+            {display === "WK" && <Image src="/WhiteKing.png" alt={display} width={pieceWidth} height={pieceHeight} />}
+            {display === "WQ" && <Image src="/WhiteQueen.png" alt={display} width={pieceWidth} height={pieceHeight} />}
+            {display === "WB" && <Image src="/WhiteBishop.png" alt={display} width={pieceWidth} height={pieceHeight} />}
+            {display === "WR" && <Image src="/WhiteRook.png" alt={display} width={pieceWidth} height={pieceHeight} />}
+            {display === "WN" && <Image src="/WhiteKnight.png" alt={display} width={pieceWidth} height={pieceHeight} />}
+            {display === "WP" && <Image src="/WhitePawn.png" alt={display} width={pieceWidth} height={pieceHeight} />}
+            {display === "BK" && <Image src="/BlackKing.png" alt={display} width={pieceWidth} height={pieceHeight} />}
+            {display === "BQ" && <Image src="/BlackQueen.png" alt={display} width={pieceWidth} height={pieceHeight} />}
+            {display === "BB" && <Image src="/BlackBishop.png" alt={display} width={pieceWidth} height={pieceHeight} />}
+            {display === "BR" && <Image src="/BlackRook.png" alt={display} width={pieceWidth} height={pieceHeight} />}
+            {display === "BN" && <Image src="/BlackKnight.png" alt={display} width={pieceWidth} height={pieceHeight} />}
+            {display === "BP" && <Image src="/BlackPawn.png" alt={display} width={pieceWidth} height={pieceHeight} />}
         </button>
     );
 }
