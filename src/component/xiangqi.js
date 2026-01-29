4484886099b9890d7382dbb1acf0bae8f743e4aa
RisingSatalite <saihajmann07@gmail.com>
2026-01-29 11:47:57 -0700

    fix white backrow display

--- src/component/xiangqi.js
+++ src/component/xiangqi.js
@@ -18,6 +18,7 @@ export default function XiangqiChess() {
   ]);
 
   const boardLenght = 9
+  const boardHeight = 9
   const boardSquareCount = 90
   
   const [turn, setTurn] = useState("W");
@@ -886,7 +887,7 @@ export default function XiangqiChess() {
           {gameStatus === "check" && <p style={{color: "orange", fontWeight: "bold"}}>CHECK!</p>}
           {gameStatus === "stalemate" && <p style={{color: "blue", fontWeight: "bold"}}>STALEMATE!</p>}
         </div>
-        {Array.from({ length: Math.ceil(board.length / 10) }, (_, rowIndex) => (
+        {Array.from({ length: Math.ceil(board.length / boardHeight) }, (_, rowIndex) => (
           <div key={rowIndex} className="row">
             {board.slice(rowIndex * boardLenght, rowIndex * boardLenght + boardLenght).map((item, index) => (
               <Square
