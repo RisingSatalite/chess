16fad11940eefe89d6528910113b7936ccf07a37
Saihaj <saihajmann07@gmail.com>
2026-08-31 12:44:33 -0600

    no queen

--- src/component/shogi.js
+++ src/component/shogi.js
@@ -349,12 +349,6 @@ export default function Chess() {
       } else {
         return ineligableMoveClear()
       }
-    }else if(board[selectedSquare1][1] === 'Q') {
-      if (((horizontallyConnecting() && noGhostingHorizontal()) || (connectingBishop() && noGhostingDiagonal())) && noFriendlyFire()) {
-        return true;
-      } else {
-        return ineligableMoveClear()
-      }
     }else if(board[selectedSquare1][1] === 'K') {
       //console.log("Can castle?" + checkCastle())
       if ((canKingAttack(selectedSquare1, selectedSquare2) && noFriendlyFire())) {
